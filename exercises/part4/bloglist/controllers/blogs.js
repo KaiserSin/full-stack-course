8cc63c575e23c5d18b56985203423ab574977a3d
KaiserSin <arten.smernov@bk.ru>
2025-06-14 15:54:22 +0300

    4.17

--- exercises/part4/bloglist/controllers/blogs.js
+++ exercises/part4/bloglist/controllers/blogs.js
@@ -3,17 +3,28 @@ const blogsRouter = require('express').Router()
 
 const { request } = require('http')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const { response } = require('../app')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+
+    const count = await User.countDocuments()
+    const randomIndex = Math.floor(Math.random() * count)
+    const user = await User.findOne().skip(randomIndex)
+
+    const blog = new Blog({
+        ...request.body,
+        user: user._id
+    })
 
     const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
     response.status(201).json(result)
 })
 
