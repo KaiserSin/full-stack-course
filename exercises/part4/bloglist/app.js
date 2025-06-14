8a5b3b14fed657c4102b21b8059d2d30357825c1
KaiserSin <arten.smernov@bk.ru>
2025-06-14 14:30:07 +0300

    4.15

--- exercises/part4/bloglist/app.js
+++ exercises/part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
